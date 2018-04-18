6bdcaec1c2ecc57b9cf56c284dbdd06e0aa38c91
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-18 21:49:59 +0300

    change the doman name

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.get('**', (req, res) => {
-    res.redirect('https://anghami.gq');
+    res.redirect('https://anghamify.cf');
 });
 
 app.listen(port, console.log('Server is running'));
