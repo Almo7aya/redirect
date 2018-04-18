bc371f824a8019dddb30df95e7a5f9dc6fe21128
Ali Almohaya <AliAlmohaya@gmail.com>
2018-04-18 22:11:05 +0300

    add path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.get('**', (req, res) => {
-    res.redirect('https://anghamify.cf');
+    res.redirect('https://anghamify.cf' + req.path);
 });
 
 app.listen(port, console.log('Server is running'));
